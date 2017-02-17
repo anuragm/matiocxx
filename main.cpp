2ee7dddad4240ec73ef2a7db89ee77233249a25c
Anurag Mishra <anuragmi@usc.edu>
2017-02-16 19:59:11 -0800

    Add example cases for Armadillo matrix types

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "matiocxx.hpp"
+#include <armadillo>
 #include <iostream>
 
 int main()
@@ -6,15 +7,18 @@ int main()
     double myDataDouble[] = {1.05,2.05,3.05};
     int myDataInt[] = {1,2,3};
     std::vector<int> intVec(myDataInt,myDataInt+3);
+    arma::mat arma_mat(10,10,arma::fill::eye);
 
     matio::matvar<double> myVarDouble("myDataDouble",{1,3},myDataDouble);
     matio::matvar<int>    myVarInt("myDataInt",{1,3},myDataInt);
     matio::matvar<int>    var_stdvec("std_vec_int",intVec);
     matio::matvar<char>   var_string("myString","This is a test string");
+    matio::matvar<double> var_arma("arma_eye",arma_mat);
+    matio::matvar<double> var_arma_vec("arma_vec",arma::vec(10,arma::fill::ones));
 
     matio::matfile myFile("test.mat");
 
-    myFile << myVarDouble << myVarInt << var_stdvec <<var_string;
+    myFile << myVarDouble << myVarInt << var_stdvec <<var_string << var_arma <<var_arma_vec;
     myFile.write();
 
     return 0;
--- matiocxx_bits/matiocxx_matfile.hpp
+++ matiocxx_bits/matiocxx_matfile.hpp
@@ -2,18 +2,14 @@
 
 void matfile::write(){                     //And write to disk
     mat_t* fp = Mat_CreateVer(fileName.c_str(),NULL,MAT_FT_MAT73);
-    std::printf("Opened file to write! \n");
 
     for(auto& varp : varArray){
         auto temp_ptr = varp.get();
-        std::printf("Got the variable pointer! \n");
 
         if( NULL != temp_ptr)
             Mat_VarWrite(fp,temp_ptr,MAT_COMPRESSION_NONE);
         else
             std::printf("Cannot write a null pointer!");
-
-        std::printf("Write a variable to disk!! \n");
     }
 
     Mat_Close(fp);
